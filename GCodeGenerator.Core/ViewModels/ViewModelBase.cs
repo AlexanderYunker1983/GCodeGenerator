55cfce7adb44402fde4088f03ac77341fec1c06c
alexy <alexy@local.yurion.ru>
2025-12-21 12:41:47 +0300

    Шаблон пустого приложения

---
+++ GCodeGenerator.Core/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace GCodeGenerator.Core.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ GCodeGenerator.Core/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GCodeGenerator.Core.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
