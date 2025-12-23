ff8b5db2922a67db6bcddd9f3e207f32693c78f4
alexy <alexy@local.yurion.ru>
2025-12-23 21:02:22 +0300

    Правка названий по-умолчанию

--- GCodeGenerator.Core/ViewModels/RightPanel/PrimitivesListViewModel/PrimitivesListViewModel.cs
+++ GCodeGenerator.Core/ViewModels/RightPanel/PrimitivesListViewModel/PrimitivesListViewModel.cs
@@ -34,19 +34,19 @@ public partial class PrimitivesListViewModel : ViewModelBase, IHasDisplayName
         };
 
         // Временный список примитивов с реальными данными недалеко от начала координат.
-        Primitives.Add(new PointPrimitive("Точка (0,0)", 0, 0));
-        Primitives.Add(new LinePrimitive("Линия (0,0)-(10,0)", 0, 0, 10, 0));
-        Primitives.Add(new CirclePrimitive("Круг (0,0), R=5", 0, 0, 5));
-        Primitives.Add(new RectanglePrimitive("Прямоугольник (0,0) 10x5", 0, 0, 10, 5, 0));
-        Primitives.Add(new EllipsePrimitive("Эллипс (0,0) 8x4", 0, 0, 8, 4, 0));
-        Primitives.Add(new ArcPrimitive("Дуга (0,0), R=5, 0-90°", 0, 0, 5, 0, 90));
-        Primitives.Add(new PolygonPrimitive("Многогранник (0,0), R=5, 6 граней", 0, 0, 5, 6, 0));
+        Primitives.Add(new PointPrimitive("Точка", 0, 0));
+        Primitives.Add(new LinePrimitive("Линия", 0, 0, 10, 0));
+        Primitives.Add(new CirclePrimitive("Круг", 0, 0, 5));
+        Primitives.Add(new RectanglePrimitive("Прямоугольник", 0, 0, 10, 5, 0));
+        Primitives.Add(new EllipsePrimitive("Эллипс", 0, 0, 8, 4, 0));
+        Primitives.Add(new ArcPrimitive("Дуга", 0, 0, 5, 0, 90));
+        Primitives.Add(new PolygonPrimitive("Многогранник", 0, 0, 5, 6, 0));
 
-        var dxf = new DxfPrimitive("DXF-объект (0,0)", 0, 0, "demo.dxf", 0);
+        var dxf = new DxfPrimitive("DXF-объект", 0, 0, "demo.dxf", 0);
         dxf.Children.Add(new LinePrimitive("DXF-линия", -5, -5, 5, 5));
         Primitives.Add(dxf);
 
-        var composite = new CompositePrimitive("Составной объект (5,5)", 5, 5, 0);
+        var composite = new CompositePrimitive("Составной объект", 5, 5, 0);
         composite.Children.Add(new CirclePrimitive("Вложенный круг", 0, 0, 2));
         composite.Children.Add(new RectanglePrimitive("Вложенный прямоугольник", 0, 0, 4, 2, 45));
         Primitives.Add(composite);
